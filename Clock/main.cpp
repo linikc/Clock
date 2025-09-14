319ea947d94eb77a632d4073afafaa91116d18b2
Linik <lonestarry@163.com>
2025-09-14 11:53:26 +0800

    main file update

--- Clock/main.cpp
+++ Clock/main.cpp
@@ -1,201 +1,169 @@
-#include "extend.h"
-const int delay = 1 * CLOCKS_PER_SEC;
-bool updated = false;
-bool running = true;
-enum InputState { HOR, MIN, SEC };
-std::wstring end_word = L"END";
-std::wstring tip = L"ENTER";
-ExMessage msg;
-int status;//0 regular 1 stop
-int h, m, s = 0;
+#include "Timer.h"
+#include "Display.h"
+#include "Logic.h"
+#include "Interaction.h"
+TimerStatus Status = START;
 
+//statement
+TimerMode mode = COUNT_DOWN;
+TimerLogic logic;
+TimerDrawer draw;
+Interaction inter;
+LOGFONT F;
+ExMessage msg;
+const int delay = 1 * CLOCKS_PER_SEC;
+int h, m, s;
+std::wstring tip = L"";
+clock_t start_time = clock();
+clock_t end_time = clock();
+//handle exit
 WNDPROC g_originWndProc = NULL;
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT u_msg, WPARAM wParam, LPARAM lParam)
 {
-	if (u_msg == WM_CLOSE) {
-		exit(0);
-	}
-	else {
-		LRESULT result = CallWindowProc((WNDPROC)g_originWndProc, hwnd, u_msg, wParam, lParam);
-		return result;
-	}
+    if (u_msg == WM_CLOSE) {
+        exit(0);
+    }
+    else {
+        LRESULT result = CallWindowProc((WNDPROC)g_originWndProc, hwnd, u_msg, wParam, lParam);
+        return result;
+    }
 }
+
+//statement of function
 void Init();
-void Function();
-void ioSys();
+void Graphic(int h, int m, int s, std::wstring tip);
+void Preparation();
+
+#if DEBUG
+int main();
+#else
+int WinMain(HINSTANCE hInstace, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
+#endif // DEBUG
+
 int main()
 {
-	Init();
-	drawTimeC(0, 0, 0);
-	HWND hwnd = GetHWnd();
-	g_originWndProc = (WNDPROC)GetWindowLongPtr(hwnd, GWLP_WNDPROC);
-	SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)WindowProc);
-	std::thread cl_f(Function);
-	while (true)
-	{
-		if (running == false)
-			break;
-		if (updated == true)
-		{
-			BeginBatchDraw();
-			drawTimeC(h, m, s);
-			drawTip(tip);
-			EndBatchDraw();
-			Sleep(400);
-			FlushBatchDraw();
-		}
-	}
-	cl_f.join();
-	drawTimeT(end_word, end_word, end_word);
-	Sleep(1000);
-	closegraph();
-	return 0;
+    Init();
+    BeginBatchDraw();
+    while (Status == START) {
+        Preparation();
+        logic.startTimer(h, m, s, mode);
+        while (Status == RUN || Status == STOP) {
+            start_time = clock();
+            tip = L"";
+            if (logic.isFinish())
+                break;
+            inter.GetInput(msg);
+            inter.HandleInputB();
+            logic.updateTimer();
+            logic.stopResume();
+            if (Status == STOP)
+                tip = L"Pause";
+            if (Status == RESET)
+            {
+                tip = L"RESET";
+                Graphic(h, m, s, tip);
+                logic.resetTimer();
+            }
+            logic.getCurrentTime(h, m, s);
+            Graphic(h, m, s, tip);
+            FlushBatchDraw();
+            printf("%d %d %d\n", h, m, s);
+            end_time = clock();
+            Sleep(delay - (end_time - start_time) > 0 ? delay - (end_time - start_time) : 0);
+        }
+    }
+    Sleep(100);
+    tip = L"Exiting...";
+    Graphic(h, m, s, tip);
+    printf("Exiting...");
+    FlushBatchDraw();
+    EndBatchDraw();
+    Sleep(1000);
+    return 0;
 }
-void Init()
+int WinMain(HINSTANCE hInstace, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	initgraph(WIDTH,HEIGHT,NULL);
-	graphClean();
-	fontInit();
-	status = 0;
+    Init();
+    BeginBatchDraw();
+    while (Status == START) {
+        Preparation();
+        logic.startTimer(h, m, s, mode);
+        while (Status == RUN || Status == STOP) {
+            start_time = clock();
+            tip = L"";
+            if (logic.isFinish())
+                break;
+            inter.GetInput(msg);
+            inter.HandleInputB();
+            logic.updateTimer();
+            logic.stopResume();
+            if (Status == STOP)
+                tip = L"Pause";
+            if (Status == RESET)
+            {
+                tip = L"RESET";
+                Graphic(h, m, s, tip);
+                logic.resetTimer();
+            }
+            logic.getCurrentTime(h, m, s);
+            Graphic(h, m, s, tip);
+            FlushBatchDraw();
+            end_time = clock();
+            Sleep(delay - (end_time - start_time) > 0 ? delay - (end_time - start_time) : 0);
+        }
+    }
+    Sleep(100);
+    tip = L"Exiting...";
+    Graphic(h, m, s, tip);
+    FlushBatchDraw();
+    EndBatchDraw();
+    Sleep(1000);
+    return 0;
 }
-void Function()
-{
-	wchar_t input[10];
-	ioSys();
-	updated = true;
-	graphClean();
-	tip = L"Counting...";
-	clock_t start = clock();
-	for (;;)
-	{
-		start = clock();
-		if (h * 3600 + m * 60 + s == 0)
-		{
-			running = false;
-			break;
-		}
-		while (s >= 60)
-		{
-			s -= 60;
-			m++;
-		}
-		while (m >= 60)
-		{
-			m -= 60;
-			h++;
-		}
-		if (h != 0)
-			if (m == 0)
-			{
-				m = 60;
-				h--;
-			}
-		if (s == 0)
-			if (m != 0)
-			{
-				s = 60;
-				m--;
-			}
-			else
-				break;
-		s --;
-		while (clock() - start <= delay);
-	}
+//definition of the function
+void Init() {
+    printf("Init...\n");
+#if DEBUG
+    initgraph(WIDTH, HEIGHT, SHOWCONSOLE);
+#else 
+    initgraph(WIDTH, HEIGHT);
+#endif
+    gettextstyle(&F);
+    F.lfHeight = FWIDTH;
+    settextstyle(&F);
+    HWND hwnd = GetHWnd();
+    g_originWndProc = (WNDPROC)GetWindowLongPtr(hwnd, GWLP_WNDPROC);
+    SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR)WindowProc);
+    draw.CleanCanva();
+    draw.DrawLandscape();
 }
-void ioSys()
-{
-	InputState input_state = HOR;
-	std::string HORINPUT = "";
-	std::string MININPUT = "";
-	std::string SECINPUT = "";
-	while (true)
-	{
-		if (peekmessage(&msg,EX_KEY))
-		{
-			if (msg.message == WM_KEYDOWN)
-			{
-				if (msg.vkcode >= '0' && msg.vkcode <= '9')
-				{
-					char num = (char)msg.vkcode;
-					switch (input_state)
-					{
-					case HOR:
-						if (HORINPUT.length() < 2)
-						{
-							HORINPUT += num;
-							h = std::stoi(HORINPUT);
-						}
-						break;
-					case MIN:
-						if (MININPUT.length() < 2)
-						{
-							MININPUT += num;
-							m = std::stoi(MININPUT);
-						}
-						break;
-					case SEC:
-						if (SECINPUT.length() < 2)
-						{
-							SECINPUT += num;
-							s = std::stoi(SECINPUT);
-						}
-						break;
-					}
-				}
-				if (msg.vkcode == VK_BACK)
-				{
-					switch (input_state)
-					{
-					case HOR:
-						if (!HORINPUT.empty())
-						{
-							HORINPUT = "";
-							h = 0;
-						}
-						break;
-					case MIN:
-						if (!MININPUT.empty())
-						{
-							MININPUT = "";
-							m = 0;
-						}
-						break;
-					case SEC:
-						if (!SECINPUT.empty())
-						{
-							SECINPUT = "";
-							s = 0;
-						}
-						break;
-					}
-				}
-				if (msg.vkcode == VK_RIGHT)//->
-					input_state = (InputState)((input_state + 1) % 3);
-				if (msg.vkcode == VK_LEFT)
-					input_state = (InputState)((input_state + 2) % 3);
-				switch (input_state)
-				{
-				case HOR:
-					tip = L"HOR";
-					break;
-				case MIN:
-					tip = L"MIN";
-					break;
-				case SEC:
-					tip = L"SEC";
-					break;
-				}
-				if (msg.vkcode == 13)
-					return;
-				if (input_state == SEC && SECINPUT.length() == 2)
-					return;
-			}
-		}
-		BeginBatchDraw();
-		drawTimeC(h, m, s);
-		drawTip(tip);
-		EndBatchDraw();
-		Sleep(10);
-		FlushBatchDraw();
-	}
+
+void Graphic(int h, int m, int s, std::wstring tip) {
+    draw.DrawLandscape();
+    draw.DrawClock(h, m, s);
+    draw.DrawTip(tip, F);
+}
+
+void Preparation() {
+    h = 0, m = 0, s = 0;
+    while (Status == START) {
+        inter.GetInput(msg);
+        inter.HandleInputA();
+        if (Status == RUN) {
+            inter.ReturnInput(s);
+            break;
+        }
+        switch (inter.GetInputState()) {
+        case HOR:
+            inter.ReturnInput(h); tip = L"HOR"; break;
+        case MIN:
+            inter.ReturnInput(m); tip = L"MIN"; break;
+        case SEC:
+            inter.ReturnInput(s); tip = L"SEC"; break;
+        }
+        Graphic(h, m, s, tip);
+        FlushBatchDraw();
+        Sleep(100);
+    }
+    tip = L"";
 }
